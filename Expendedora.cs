f630fae0a423a625b709a72492d3fa2e3d25e477
Alumno <Alumno@L21-2543315>
2023-08-23 11:48:32 -0600

    Agregar archivos de proyecto.

---
+++ Expendedora.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace ExpendedoraG2_2024_1
+{
+    internal class Expendedora
+    {
+        #region Atributos
+        private string marca;
+        private ushort cantproductos;
+        private float precio;
+        #endregion
+
+        #region Métodos
+        private void Saludar()
+        {
+            Console.WriteLine("Bienvenido. Elige un producto");
+        }
+
+        private void LimpiarDisplay()
+        {
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+        public string MostrarCodProducto()
+        {
+            Console.WriteLine("3A: Doritos\n3B: Churrumais");
+            Console.WriteLine("Ingresa el código del producto:");
+            string codigo = Console.ReadLine();
+            return codigo;
+        }
+
+        public void MostrarPrecio(string codigo)
+        {
+            switch(codigo)
+            {
+                case "3A":
+                    Console.WriteLine("Precio: ${0}",precio);
+                    break;
+                case "3B":
+                    Console.WriteLine("Precio: ${0}",precio-6);
+                    break;
+                default:
+                    Console.WriteLine("No ingresaste un código válido");
+                    break;
+
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Expendedora()
+        {
+            marca = "AMS";
+            precio = 18;
+            Saludar();
+            LimpiarDisplay();
+            Console.WriteLine("Marca: {0}", marca);
+            LimpiarDisplay();
+            string codigo=MostrarCodProducto();
+            LimpiarDisplay();
+            MostrarPrecio(codigo);
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ExpendedoraG2_2024_1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Expendedora miExpendedora = new Expendedora();
+    }
+}
